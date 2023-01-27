818e6db093c4ea12701c167a6a222b4e18d28aea
Elkinews <Elkinsuarezews@gmail.com>
2023-01-27 16:14:07 -0500

    fixed

---
+++ tests/Database/Factories/UserFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Coderflex\LaravelTicket\Tests\Database\Factories;
+
+use Coderflex\LaravelTicket\Tests\Models\User;
+
+class UserFactory extends \Orchestra\Testbench\Factories\UserFactory
+{
+    protected $model = User::class;
+}
